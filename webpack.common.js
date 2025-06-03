fec4bdad092de0a7721b0f66b69bda99d4fd4201
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-02 23:22:51 -0700

    Implemented webpack-merge, separated dev/prod modes

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -27,7 +22,7 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: "html-loader",
-    }
+      }
     ],
   },
 };
